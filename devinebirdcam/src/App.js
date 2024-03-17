851cae0f7878ea3055bd8363c895222ae0e8b1be
Travis Church <thehaab@gmail.com>
2024-03-16 20:05:14 -0500

    update

--- devinebirdcam/src/App.js
+++ devinebirdcam/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import BirdList from './components/birdlist/BirdList';
 import Webcam from './components/webcam/Webcam';  
+import Header from './components/header/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <BirdList />
       <Webcam />
     </>
---
+++ devinebirdcam/src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Devine Bird Cam</h1>
+      <navbar>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </navbar>
+    </header>
+  );
+};
+
+export default Header;
